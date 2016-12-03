c0dd6c02bb8aa2056be4371fdf5f83cc0fe59cf7
eimantas <eimantas.tauklys@virtustream.com>
2016-12-03 15:02:01 +0200

    Working optimal slider gallery

--- pub/gallery-fast.jsx
+++ pub/gallery-fast.jsx
@@ -1,7 +1,57 @@
 const galleryComp = React.createClass({
-    render() {
-        return <div id="gallery" className="fast">
-            {_.map(this.props.images, image => <img src={image.low_resolution.url}/>)}
-        </div>
-    }
+	getInitialState: function () {
+		return {
+			images: this.props.images,
+			currentImage: 0
+		};
+	},
+	componentDidMount: function () {
+		window.addEventListener('scroll', this.handleScroll);
+	},
+
+	componentWillUnmount: function () {
+		window.removeEventListener('scroll', this.handleScroll);
+	},
+
+	handleScroll: function (event) {
+		var currentYPosition = window.scrollY;
+		var imageSize = window.innerHeight;
+
+		const startingIndex = Math.floor(currentYPosition / imageSize - 3)
+
+
+		this.setState({
+			currentImage: startingIndex > 0 ? startingIndex : 0
+		});
+	},
+
+	translateImageStyle(imageIndex){
+		return {
+			transform: `translate(0, ${imageIndex}00vh)`,
+		}
+	},
+
+	renderImages(initialImage){
+		var renderedImages = [];
+		for (var currentImgIndex = initialImage; (currentImgIndex < initialImage + 7) && (currentImgIndex < this.props.images.length)
+		&& this.props.images[currentImgIndex].low_resolution; currentImgIndex++) {
+			renderedImages.push(<img key={currentImgIndex}
+			                         style={this.translateImageStyle(currentImgIndex)}
+			                         src={this.props.images[currentImgIndex].low_resolution.url}/>);
+		}
+		return renderedImages;
+	},
+	setGaleryHeight(){
+		return {
+			height: this.state.images.length + "00vh",
+		}
+	},
+
+	render() {
+		return (
+			<div id="gallery" className="fast" style={this.setGaleryHeight()}>
+				{this.renderImages(this.state.currentImage)}
+			</div>
+		)
+	}
 });
